9bfc29fe30ccd52a25783ed201051dab325db50f
jmtblei <47536941+jmtblei@users.noreply.github.com>
2021-04-07 02:26:14 -0700

    filter/search bar

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,44 +1,72 @@
 import React from 'react'
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import ProductCard from "../components/ProductCard";
 import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const HomeScreen = () => {
+  const [search, setSearch] = useState("");
+  const [filteredBoosters, setFilteredBoosters] = useState([]);
+
   const dispatch = useDispatch();
 
   const getProducts = useSelector((state) => state.getProducts);
   const { products, loading, error } = getProducts;
 
   useEffect(() => {
-    dispatch(listProducts());
+    dispatch(listProducts())
   }, [dispatch]);
+
+  useEffect(() => {
+    setFilteredBoosters(
+      products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, products]);
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value)
+    console.log(e.target.value)
+  };
 
   return (
     <div className="homescreen">
-      <h2 className="homescreen-title">Draft Boosters For Sale</h2>
+      <div className="homescreen-upper">
+        <h2 className="homescreen-title">Draft Boosters For Sale</h2>
+        <div className="homescreen-search">
+          <input 
+            type="text"
+            placeholder="Search Booster Tutor"
+            onChange={searchHandler}
+          />
+          <i class="fas fa-search"></i>
+        </div>
+      </div>
       <div className="homescreen-products">
         {loading ? (
           <h2>Loading...</h2>
         ) : error ? (
           <h2>{error}</h2>
         ) : (
-          products.map((product) => (
-            <ProductCard
-              key={product._id}
-              name={product.name}
-              description={product.description}
-              price={product.price}
-              imageURL={product.imageURL}
-              logoURL={product.logoURL}
-              setType={product.setType}
-              block={product.block}
-              releaseDate={product.releaseDate}
-              countInStock={product.countInStock}
-              productId={product._id}
-            />
-          ))
+          <>
+            {filteredBoosters.map((product) => (
+              <ProductCard
+                key={product._id}
+                name={product.name}
+                description={product.description}
+                price={product.price}
+                imageURL={product.imageURL}
+                logoURL={product.logoURL}
+                setType={product.setType}
+                block={product.block}
+                releaseDate={product.releaseDate}
+                countInStock={product.countInStock}
+                productId={product._id}
+              />
+            ))}
+          </>
         )}
       </div>
     </div>
