d0ba9a89587a149133aabcb247fa7519a27ae978
jmtblei <jmtblei@gmail.com>
2022-03-07 20:10:38 -0800

    banner

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,7 @@ const HomeScreen = () => {
     return (
        <div className="homescreen">
            <div className="homescreen-banner">
-               <img src="https://mtgboosters.s3.us-west-1.amazonaws.com/boostertutor.jpg" alt=""/>
+               {/* <img src="https://mtgboosters.s3.us-west-1.amazonaws.com/boostertutor.jpg" alt=""/> */}
                <img src="https://mtgboosters.s3.us-west-1.amazonaws.com/boostertutor2.jpg" alt="" className="banner2"/>
            </div>
            <FeaturedCarousel />
