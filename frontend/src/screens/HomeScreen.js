7bc2932fc949226dbfcff1b223726af47e91f4c2
jmtblei <jmtblei@gmail.com>
2022-03-29 01:26:23 -0700

    banner text

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import FeaturedCarousel from '../components/FeaturedCarousel';
 
 const HomeScreen = () => {
 
     return (
        <div className="homescreen">
-           <div className="homescreen-banner">
-               {/* <img src="https://mtgboosters.s3.us-west-1.amazonaws.com/boostertutor.jpg" alt=""/> */}
+           <Link to="/products" className="homescreen-banner">
                <img src="https://mtgboosters.s3.us-west-1.amazonaws.com/boostertutor2.jpg" alt=""/>
-           </div>
+               <h2 className="homescreen-banner-center-text">SHOP BOOSTERS NOW!</h2>
+           </Link>
            <FeaturedCarousel />
        </div>
     );
